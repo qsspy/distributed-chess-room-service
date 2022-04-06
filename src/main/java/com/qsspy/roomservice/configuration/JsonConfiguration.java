30fe3552092615f254bb080f1704794293527c5f
Daniel <danel.filipek@gmail.com>
2022-04-07 00:11:19 +0200

    Revert "[TEST] - Added dummy log to verify docker hub pipeline"
    This reverts commit cafe00d7

--- src/main/java/com/qsspy/roomservice/configuration/JsonConfiguration.java
+++ src/main/java/com/qsspy/roomservice/configuration/JsonConfiguration.java
@@ -2,17 +2,14 @@ package com.qsspy.roomservice.configuration;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Slf4j
 public class JsonConfiguration {
 
     @Bean
     public ObjectMapper objectMapper() {
-        log.error("It works :)!");
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return objectMapper;
